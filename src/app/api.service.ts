0ca0fb20e095808b900b4073542f5d028b4a6582
aman_agrawal <aman.agarwal@timesinternet.in>
2018-10-28 14:24:07 +0530

    First commit to pwa app using angular

---
+++ src/app/api.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, ObservableLike } from 'rxjs';
+
+export interface Item {
+  title: string;
+  url: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  private dataURL = 'https://api.hnpwa.com/v0/news/1.json';
+
+  constructor(private httpClient: HttpClient) {}
+
+  getData(): Observable<Item[]> {
+    return <Observable<Item[]>>this.httpClient.get(this.dataURL);
+  }
+}
